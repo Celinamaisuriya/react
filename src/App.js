b70b7ac46df503a6ec84c4419460cab670817f71
Celinamaisuriya <114462161+Celinamaisuriya@users.noreply.github.com>
2024-02-08 16:05:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
 import './App.css';
-// import Greet from './components/Greet';
 
+// import Greet from './components/Greet';
 // import { Greet } from './components/Greet' //named export
+
 // import Welcome from './components/Welcome';
+
 // import GreetProp from './components/GreetProp';
+
 // import StateMessage from './components/StateMessage';
+
 // import Counter from './components/Counter';
+
 // import FunctionClick from './components/FunctionClick'
+
 import ClassClick from './components/ClassClick'
 
 class App extends Component{
@@ -25,10 +31,11 @@ class App extends Component{
 
         <GreetProp name="Diana" heroname="Wonder Woman" />*/} 
         
-        {/* <Welcome name="Bruce" heroname="Batman" />
+        {/* 
+        <Welcome name="Bruce" heroname="Batman" />
         <Welcome name="clark" heroname="Superman" />
         <Welcome name="Diana" heroname="Wonder Woman" /> 
-         */}
+        */}
 
          {/* <StateMessage /> */} 
 
